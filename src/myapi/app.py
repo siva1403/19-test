4d26fcb4532cdc71289fcbcd9eda5db3f5c4c9fc
Nagabharath1234 <bharathchowdhary79@gmail.com>
2026-06-19 14:46:47 +0530

    Update home route response with edited_by message

--- src/myapi/app.py
+++ src/myapi/app.py
@@ -3,15 +3,14 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
     return jsonify({
         "message": "Python API is running",
-        "message": "Edited by bharath",
-        "timestamp": datetime.now().strftime("%H:%M:%S %d %b %Y")
+        "edited_by": "bharath",
+        "timestamp": datetime.now().strftime("%H:%M:%S %d %b %Y"),
+        "message": "edited by vijay"
     })
-
 
 @app.route("/health")
 def health():
@@ -19,10 +18,8 @@ def health():
         "status": "UP"
     })
 
-
 def main():
     app.run(host="0.0.0.0", port=5000)
-
 
 if __name__ == "__main__":
     main()
